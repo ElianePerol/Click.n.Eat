318e1f3bebb34a1b93a81c95754e2f15456d98de
Eliane <eliane.perol@gmail.com>
2025-01-29 12:41:54 +0100

    second commit

--- ClicknEat/database/seeders/DatabaseSeeder.php
+++ ClicknEat/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Restaurant;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,21 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Manually adds one line in the table
+        // Restaurant::create([
+        //     'name' => 'Punjab'
+        // ]);
+
+        // use the factory create to hydrate the table with fake entries
+
+        Restaurant::factory(1000)->create();
+        
+        Category::factory(10)->create();
+
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
-#ClicknEat
+# ClicknEat
 
 Application permettant la réservation de tables en restaurant, avec la possibilité de passer commande à l'avance. Grâce à ce système, les clients sont servis dès leur arrivée, offrant une expérience fluide et rapide.
+
+## Models
+- Restaurant
+ - name
+
+- Client
+ - name
+
+- Category ==> TODO + SEEDER
+ - name
